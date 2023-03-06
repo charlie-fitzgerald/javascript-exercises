8d4b4c617c9139996a37da6ab4adbfd019edcfd4
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-03-05 16:37:45 -0800

    Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(memberNum) {
+    //calculate fibonacci sequence up to sequence member number given
+    let fibArray = [1, 1];
 
+    let num = parseInt(memberNum);
+
+    if (num < 1) {
+        return 'OOPS';
+    }
+
+    for (let i = 0; i < num; i++) {
+        let newFibNum = fibArray[fibArray.length -1] + fibArray[fibArray.length -2];
+        fibArray.push(newFibNum);
+    }
+
+
+    //return the number from the sequence
+    return fibArray[num - 1];
 };
 
 // Do not edit below this line
